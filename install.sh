cb692ff6f4f20065965ebca714469b1b7cb884c4
Perrin Samuels <perrin@descartes>
2024-02-16 11:30:27 -0800

    restore functionality

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#cat nakakatawang-pag-download/x?? > Mac_TurboTax_Download.dmg
-echo open Mac_TurboTax_Download.dmg
+cat nakakatawang-pag-download/x?? > Mac_TurboTax_Download.dmg
+open Mac_TurboTax_Download.dmg
